9b5ce0e5cbac540434089b314fd52a24dbe05cea
Osiris30 <andradeivania16@gmail.com>
2025-10-15 00:06:50 -0600

    corrigiendo pequeño error

--- mvc/app/controllers/Dia1Controller.php
+++ mvc/app/controllers/Dia1Controller.php
@@ -20,32 +20,26 @@ class Dia1Controller {
                     "/mvc/public/images/bitcoin5.jpg",
                     "/mvc/public/images/bitcoin6.jpg",
                     "/mvc/public/images/bitcoin7.jpg"
-                
                 ],
-                "video" => "" 
+                "video" => ""
             ],
             [
-        "titulo" => "Segunda Ponencia - Inteligencia Artificial",
-        "descripcion" => "Rodrigo Pineda.",
-        "imagenes" =>  [,
-                        "/mvc/public/images/artificial2.jpg",
-                        "/mvc/public/images/artificial3.jpg",
-                        "/mvc/public/images/artificial4.jpg",
-                        "/mvc/public/images/artificial5.jpg",
-                        "/mvc/public/images/artificial6.jpg",
-                        "/mvc/public/images/artificial7.jpg",
-                        "/mvc/public/images/artificial8.jpg",
-
-
-        ],
-        "video" => ""
-    ]
-
-            
+                "titulo" => "Segunda Ponencia - Inteligencia Artificial",
+                "descripcion" => "Rodrigo Pineda.",
+                "imagenes" => [
+                    "/mvc/public/images/artificial2.jpg",
+                    "/mvc/public/images/artificial3.jpg",
+                    "/mvc/public/images/artificial4.jpg",
+                    "/mvc/public/images/artificial5.jpg",
+                    "/mvc/public/images/artificial6.jpg",
+                    "/mvc/public/images/artificial7.jpg",
+                    "/mvc/public/images/artificial8.jpg"
+                ],
+                "video" => ""
+            ]
         ];
 
         require_once __DIR__ . '/../views/dia1.php';
     }
 }
-
-?>"/mvc/public/images/artificial1.jpg"
+?>
--- mvc/app/controllers/Dia2Controller.php
+++ mvc/app/controllers/Dia2Controller.php
@@ -3,36 +3,34 @@ class Dia2Controller {
     public function index() {
         $actividades = [
             [
-                "titulo" => "Taller #1 Toma de decisiones estrategicas basadas en datos",
+                "titulo" => "Taller #1 Toma de decisiones estratégicas basadas en datos",
                 "descripcion" => "Enrique Cortez.",
                 "imagen" => [
                     "/mvc/public/images/decisiones.jpg",
                     "/mvc/public/images/decisiones1.jpg"
-
                 ],
                 "video" => ""
             ],
             [
-                "titulo" => "Taller #2 Construyendo un CRUD en minutos ",
+                "titulo" => "Taller #2 Construyendo un CRUD en minutos",
                 "descripcion" => "Ing. Gabriel Castillo",
                 "imagen" => [
-                     "/mvc/public/images/crud1.jpg",
-                        "/mvc/public/images/crud2.jpg",
-                        "/mvc/public/images/crud3.jpg",
-
+                    "/mvc/public/images/crud1.jpg",
+                    "/mvc/public/images/crud2.jpg",
+                    "/mvc/public/images/crud3.jpg"
                 ],
                 "video" => ""
             ],
-            "titulo" => "Taller #3 IA Multimodal- Creando el futuro visual e inteligente con n8n",
-                "descripcion" => "José Salmeron",
+            [
+                "titulo" => "Taller #3 IA Multimodal - Creando el futuro visual e inteligente con n8n",
+                "descripcion" => "José Salmerón",
                 "imagen" => [
                     "/mvc/public/images/multimodal.jpg",
                     "/mvc/public/images/multimodal1.jpg",
-                    "/mvc/public/images/multimodal2jpg",
-
+                    "/mvc/public/images/multimodal2.jpg"
                 ],
                 "video" => ""
-
+            ]
         ];
 
         require_once __DIR__ . '/../views/dia2.php';
